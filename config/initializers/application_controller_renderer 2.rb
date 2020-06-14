819466bef83792ce915c2e6e1a43109b55e58ea0
嘉味田崇 <kamitatakashi@kamidatakashinoMacBook-Air.local>
2020-06-14 13:47:14 +0900

    branch

---
+++ config/initializers/application_controller_renderer 2.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'f2e84736e527b1086ecb4c3e5c341cc1997428556b4d866590b3a217a4ef582d0ed7db9eb4efb47e99ab13590d5e698a1f9e68afa7f04a51689d49aacc9ab0f8'
